16496395301a023510091211120d59898aa3ec14
Sofia Dailoff <s.sdailoff@gmail.com>
2023-06-05 19:39:59 -0300

    model perfil

--- cabaniapp/perfil/models.py
+++ cabaniapp/perfil/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Perfil(models.Model):
+    nombre = models.CharField(max_length=150)
+    apellido = models.CharField(max_length=150)
+    dni = models.CharField(max_length=150)
+    fecha_nacimiento = models.DateField(max_length=10)
+    telefono = models.CharField(max_length=150)
