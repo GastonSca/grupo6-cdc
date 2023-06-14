d31efed45c67341314505b95d74eab192aab3fc4
Sofia Dailoff <sofia.dailoff1@gmail.com>
2023-06-14 17:10:53 -0300

    urls

--- cabaniapp/cabaniapp/urls.py
+++ cabaniapp/cabaniapp/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('principal.urls')),
     path('principal/', include('principal.urls')),
     path('perfil/', include('perfil.urls')),
 
